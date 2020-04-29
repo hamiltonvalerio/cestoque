3d0437535002fdd2e67c939cf979e2e499951803
hamiltonvalerio <hamilton@valerio.eti.br>
2020-04-29 09:25:50 -0300

    Projeto criado

---
+++ src/main/java/br/ipen/cestoque/CestoqueApplication.java
@@ -0,0 +1,13 @@
+package br.ipen.cestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CestoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CestoqueApplication.class, args);
+	}
+
+}
