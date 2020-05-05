462d53fc836fa58b67fcbb715312f623a2df63a3
hamiltonvalerio <hamilton@valerio.eti.br>
2020-05-05 12:42:00 -0300

    Cliente com emails SET

---
+++ src/main/java/br/ipen/cestoque/domain/Cliente.java
@@ -0,0 +1,128 @@
+package br.ipen.cestoque.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String nome;
+	
+	private String cpf;
+	
+	@ElementCollection
+	@CollectionTable(name = "EMAIL")
+	private Set<String> emails = new HashSet<>();
+	
+	private String usualt;
+	
+	private Date datalt;
+
+	public Cliente() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Cliente(Integer id, String nome, String cpf, String usualt, Date datalt) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.usualt = usualt;
+		this.datalt = datalt;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Set<String> getEmails() {
+		return emails;
+	}
+
+	public void setEmails(Set<String> emails) {
+		this.emails = emails;
+	}
+
+	public String getUsualt() {
+		return usualt;
+	}
+
+	public void setUsualt(String usualt) {
+		this.usualt = usualt;
+	}
+
+	public Date getDatalt() {
+		return datalt;
+	}
+
+	public void setDatalt(Date datalt) {
+		this.datalt = datalt;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
---
+++ src/main/java/br/ipen/cestoque/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package br.ipen.cestoque.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.ipen.cestoque.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+}
