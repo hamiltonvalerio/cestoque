f34c6e91e89d0922dd9314c5638f572fee4eeab4
hamiltonvalerio <hamilton@valerio.eti.br>
2020-05-08 07:58:05 -0300

    Metodo POST Categoria

--- src/main/java/br/ipen/cestoque/resources/CategoriaResource.java
+++ src/main/java/br/ipen/cestoque/resources/CategoriaResource.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,10 +30,11 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(categoria);
 	}
 	
-	public ResponseEntity<Void> insert(Categoria cat){
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria cat){
 		cat = service.insert(cat); 
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cat.getId()).toUri();
-		return null;
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
