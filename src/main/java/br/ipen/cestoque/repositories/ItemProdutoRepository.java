64110fa88f69bca2321b7c16bee6db9fc1174357
hamiltonvalerio <hamilton@valerio.eti.br>
2020-05-09 08:47:58 -0300

    Ajuste repository

--- src/main/java/br/ipen/cestoque/repositories/ItemProdutoRepository.java
+++ src/main/java/br/ipen/cestoque/repositories/ItemProdutoRepository.java
@@ -3,7 +3,6 @@ package br.ipen.cestoque.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.ipen.cestoque.domain.Categoria;
 import br.ipen.cestoque.domain.ItemProduto;
 
 @Repository
