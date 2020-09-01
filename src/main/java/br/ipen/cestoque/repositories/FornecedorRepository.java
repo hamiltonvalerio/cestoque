7980bb54459765dc188c9b1f08e48dad331f6aad
Hamilton <htecmac@gmail.com>
2020-09-01 12:53:01 -0300

    Busca por Nome - Fornecedor

--- src/main/java/br/ipen/cestoque/repositories/FornecedorRepository.java
+++ src/main/java/br/ipen/cestoque/repositories/FornecedorRepository.java
@@ -1,5 +1,7 @@
 package br.ipen.cestoque.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import br.ipen.cestoque.domain.Fornecedor;
 @Repository
 public interface FornecedorRepository extends JpaRepository<Fornecedor, Integer>{
 
+	public List<Fornecedor> findByNome(String nome);
+	
 }
