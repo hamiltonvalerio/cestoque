9afd13905363a71367c27b501906e0c8ced00518
hamiltonvalerio <hamilton@valerio.eti.br>
2021-05-25 07:53:07 -0300

    Jasper Config

---
+++ src/main/java/br/ipen/cestoque/config/JasperConfig.java
@@ -0,0 +1,19 @@
+package br.ipen.cestoque.config;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasperConfig {
+	
+	@Bean
+	public Connection connection(DataSource dataSource) throws SQLException {
+		return dataSource.getConnection();
+	}
+
+}
